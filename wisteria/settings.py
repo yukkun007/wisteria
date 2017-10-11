0671aaa967eec818370a447ca78caed73245031f
kato.yutaka <kato.yutaka@gmail.com>
2017-10-11 10:28:37 +0900

    django設定調整

--- wisteria/settings.py
+++ wisteria/settings.py
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'js'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
