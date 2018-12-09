e7d4cf34b4f482d823a4dce7ff6bcfbf5de4e56f
yukkun007 <kato.yutaka@gmail.com>
2018-12-09 18:02:10 +0900

    delete unuse code

--- kbot/google/gmail.py
+++ kbot/google/gmail.py
@@ -9,18 +9,11 @@ from kbot.google.gmail_api import GmailApi
 class GMail(object):
     def __init__(self):
         self.storage_path = "/tmp/st_test.txt"
-        self.secret_path = "/tmp/sc_test.txt"
-
         st = open(self.storage_path, "w")
         storage = os.environ["GMAIL_AUTH_STORAGE"]
         st.write(storage)
         st.close()
 
-        # sc = open(self.secret_path, 'w')
-        # secret = os.environ['GMAIL_CLIENT_SECRET']
-        # sc.write(secret)
-        # sc.close()
-        # secret_file = open(self.secret_path)
         auth_info = ""  # json.load(secret_file)
 
         # 初回実行時は認証が求められます
